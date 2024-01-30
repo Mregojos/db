23ecd5a70d97f19ff638281fbd00ebb39b7b45cd
Matt <mattiregojos@gmail.com>
2024-01-30 10:46:43 +0000

    Add and modify files

--- script.sh
+++ script.sh
@@ -6,3 +6,8 @@ sh manifest/deployment.sh
 
 # Create a sample psql.yaml (Deployment Kind)
 sh psql/psql-deployment.sh
+
+# Create a firewall
+sh firewall.sh
+# Delete the firewall
+sh firewall-cleanup.sh
