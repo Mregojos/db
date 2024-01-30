351fcd0ad720a3002c5110f0ac9de078a5ff8a6f
Matt <mattiregojos@gmail.com>
2024-01-30 10:47:55 +0000

    Add and modify files

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ sh manifest/deployment.sh
 
 # Create a sample psql.yaml (Deployment Kind)
 sh psql/psql-deployment.sh
+# Apply
+kubectl apply -f psql-deployment.yaml
 
 # Create a firewall
 sh firewall.sh
