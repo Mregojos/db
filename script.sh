09707b63cf0e737771093b68df2bb896dc847deb
Matt <mattiregojos@gmail.com>
2024-01-31 02:17:25 +0000

    Add and modify files

--- script.sh
+++ script.sh
@@ -7,28 +7,43 @@ sh kubectl-minikube.sh
 # Environment
 source env.sh
 
+# Create a firewall
+sh firewall.sh
+
+#---------------- Option A: Using Deployment ----------------------#
+# It's working.
 # Create a sample psql.yaml (Deployment Kind)
 sh psql/psql-deployment.sh
 sh psql/psql-service.sh
 # or
 sh psql/psql-deployment-service.sh
+
 # Apply
 kubectl apply -f psql/psql-deployment.yaml
 kubectl apply -f psql/psql-service.yaml
 # or
 kubectl apply -f psql/psql-deployment-service.yaml
+
 # Expose
 # kubectl expose deployment.apps/psql-deployment
+
 # Port-forwarding
-kubectl port-forward service/psql-service $DB_PORT:$DB_PORT --address $ADDRESS
+kubectl port-forward service/psql-service $DB_PORT:$TARGET_PORT --address $ADDRESS
+
 # Watch
 watch kubectl get all
 
 # Test the DB
 make run_test
 
+# Delete
+kubectl delete deployment/psql-deployment
+kubectl delete service/psql-service
 
-# Create a firewall
-sh firewall.sh
+#---------------- Option B: Using Statefulset ----------------------------#
+
+
+#----------- Cleanu Up ----------------------#
+
 # Delete the firewall
 sh firewall-cleanup.sh
